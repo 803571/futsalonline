0353b3ad9824fb936cbb0b5a062d3e54825bcc94
SSA <158470963+shinseongan@users.noreply.github.com>
2024-09-13 19:25:02 +0900

    Fix: 선수 데이터 삽입 수정

--- docs/players_add.sql
+++ docs/players_add.sql
@@ -2,8 +2,8 @@
 -- 해당 코드 new query에 삽입하고 mysql run
 INSERT INTO Players (name, speed, shootingFinish, shootingPower, stamina, defense, createdAt, updatedAt)
 VALUES
-('Player 1', 90, 85, 80, 75, 70, NOW(), NOW()),
-('Player 2', 85, 80, 85, 70, 75, NOW(), NOW()),
-('Player 3', 88, 78, 82, 77, 72, NOW(), NOW());
+('Player1', 0, 0, 0, 0, 0, NOW(), NOW()),
+('Player2', 0, 0, 0, 0, 0, NOW(), NOW()),
+('Player3', 0, 0, 0, 0, 0, NOW(), NOW());
 
 SELECT * FROM Players

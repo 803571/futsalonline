ab7f4a9fe695549b8d316845d4c3162c788729d7
SSA <158470963+shinseongan@users.noreply.github.com>
2024-09-15 22:10:49 +0900

    Feat: API 생성
    게임 매칭 작성 중..

--- src/game-server.js
+++ src/game-server.js
@@ -1,22 +1,15 @@
 import express from 'express';
-import dotenv from 'dotenv';
-import UsersRouter from './routes/users.router.js';
-import CashRouter from './routes/cash.router.js';
-import PlayersRouter from './routes/players.router.js';
-import RostersRouter from './routes/rosters.router.js';
 import ServerRouter from './routes/server.router.js'
 import LogMiddleware from './middlewares/log.middleware.js';
 import ErrorHandlingMiddleware from './middlewares/error-handling.middleware.js';
 
-dotenv.config();
-
 const app = express();
-const PORT = 3333;
+const PORT = 4444;
 
 app.use(LogMiddleware);
 app.use(express.json());
 
-app.use('/api', [UsersRouter, CashRouter, PlayersRouter, RostersRouter, ServerRouter]);
+app.use('/api', [ServerRouter]);
 app.use(ErrorHandlingMiddleware);
 
 app.listen(PORT, () => {

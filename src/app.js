f8e53c1dd0025a45789ec3fc2dbcc2f27b88903d
SSA <158470963+shinseongan@users.noreply.github.com>
2024-09-15 15:35:42 +0900

    Feat: API 추가
    선수 뽑기, 로스터 조회

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import UsersRouter from './routes/users.router.js';
 import CashRouter from './routes/cash.router.js';
 import PlayersRouter from './routes/players.router.js';
+import RostersRouter from './routes/rosters.router.js';
 import LogMiddleware from './middlewares/log.middleware.js';
 import ErrorHandlingMiddleware from './middlewares/error-handling.middleware.js';
 
@@ -14,7 +15,7 @@ const PORT = 3333;
 app.use(LogMiddleware);
 app.use(express.json());
 
-app.use('/api', [UsersRouter, CashRouter, PlayersRouter]);
+app.use('/api', [UsersRouter, CashRouter, PlayersRouter, RostersRouter]);
 app.use(ErrorHandlingMiddleware);
 
 app.listen(PORT, () => {
--- src/routes/cash.router.js
+++ src/routes/cash.router.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { prisma } from '../utils/prisma/index.js';
+import { getAccountCash } from '../utils/cashUtils.js'
 
 const router = express.Router();
 
@@ -64,38 +65,5 @@ router.get('/cash/get/:accountId', async (req, res, next) => {
     next(err);
   }
 });
-
-const getAccountCash = async (accountId) => {
-  try {
-    const account = await prisma.accounts.findUnique({
-      where: { accountId: +accountId },
-    });
-
-    // 계정 체크
-    if (!account) {
-      throw new Error('해당 계정을 찾을 수 없습니다.');
-    }
-
-    // 해당 계정 캐시 내역 조회
-    const cashDataset = await prisma.cashDatasets.findMany({
-      where: { accountId: +accountId },
-    });
-
-    // 거래 내역으로 보유 캐시 계산
-    const accountCash = cashDataset.reduce((acc, cash) => {
-      if (cash.type === 'charge') {
-        // 충전 시 +
-        return acc + cash.amount;
-      } else {
-        // 충전 X 시 -
-        return acc - cash.amount;
-      }
-    }, 0);
-
-    return accountCash;
-  } catch (err) {
-    throw new Error(`errorMessage: ${err.message}`);
-  }
-};
 
 export default router;
